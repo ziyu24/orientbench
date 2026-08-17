8e40454a6feb156689b0cf73f9731976c023a05b
ziyu2424 <qunchao24@sina.com>
2026-08-17 08:15:50 -0700

    server: supervise r041 RTMDet DIOR views

--- outputs/persistent_artifacts/orientbench_panorama_r041_20260817/code/rtmdet_s_dior_vflip.py
+++ outputs/persistent_artifacts/orientbench_panorama_r041_20260817/code/rtmdet_s_dior_vflip.py
@@ -5,6 +5,10 @@ _frozen = Path('/home/rspip/cqc/pro/study/pth_data/baseline_rotated_rtmdet_s_fpn
 _text = _frozen.read_text(encoding='utf-8').replace("_base_ = '../../../third_party/ai4rs/configs/rotated_rtmdet/rotated_rtmdet_s-3x-dota.py'", '')
 exec(compile(_text, str(_frozen), 'exec'))
 test_dataloader = dict(dataset=dict(
+    type=dataset_type,
+    metainfo=metainfo,
+    test_mode=True,
+    pipeline=val_pipeline,
     data_root='',
     ann_file='/home/rspip/cqc/pro/study/orientbench/top_journal_v3_reaudit_055/data_prep/DIOR/annfiles_dotaformat/test/',
     data_prefix=dict(img_path='/home/rspip/cqc/pro/study/orientbench/outputs/persistent_artifacts/orientbench_panorama_r041_20260817/input_views/dior_vflip_png/')))
