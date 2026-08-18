782437e84f620aae5d0225fdb6d44a72d9f427bd
ziyu2424 <qunchao24@sina.com>
2026-08-18 05:08:02 -0700

    server: add r043 AP75 full-eval metrics

--- top_journal_v3_reaudit_055/saur_stagea_r043_20260818/configs/dior_cont_3e.py
+++ top_journal_v3_reaudit_055/saur_stagea_r043_20260818/configs/dior_cont_3e.py
@@ -10,6 +10,8 @@ val_dataloader = dict(dataset=dict(
     ann_file='/home/rspip/cqc/pro/study/orientbench/top_journal_v3_reaudit_055/data_prep/DIOR/annfiles_dotaformat/test/',
     data_prefix=dict(img_path='/home/rspip/cqc/data/dataset/DIOR/images/test/')))
 test_dataloader = val_dataloader
+val_evaluator = dict(type='DOTAMetric', metric='mAP', iou_thrs=[.5, .75])
+test_evaluator = dict(type='DOTAMetric', metric='mAP', iou_thrs=[.5, .75])
 train_cfg = dict(max_epochs=3, val_interval=1, type='EpochBasedTrainLoop')
 load_from = '/home/rspip/cqc/pro/study/pth_data/baseline_rotated_retinanet_psc_r50_fpn_1x_le90/DIOR_trainval_test/best_mAP_5368_epoch_12.pth'
 work_dir = '/home/rspip/cqc/pro/study/orientbench/outputs/persistent_artifacts/orientbench_saur_stagea_r043_20260818/dior_cont'
