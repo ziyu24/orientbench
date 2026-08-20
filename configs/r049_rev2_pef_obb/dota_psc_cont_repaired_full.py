97aaf88ea3c504a5a5d94847ba8578cc482d2a69
ziyu2424 <qunchao24@sina.com>
2026-08-20 07:30:30 -0700

    run: add isolated repaired r049 G2 rerun

---
+++ configs/r049_rev2_pef_obb/dota_psc_cont_repaired_full.py
@@ -0,0 +1,2 @@
+_base_ = './dota_psc_cont_full.py'
+work_dir = '/home/rspip/cqc/pro/study/orientbench/outputs/persistent_artifacts/orientbench_r049_rev2_pef_obb_20260819/g2_repaired_percandidate/dota_psc_cont'
